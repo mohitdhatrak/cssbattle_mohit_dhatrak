be4a3b55c274537f411615b5dfaa848f6e9cae47
mohitdhatrak <msdhatrak25@gmail.com>
2024-04-13 23:52:39 +0530

    added basic task code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-    return <div className="App"></div>;
-}
+// external imports
+import confetti from "canvas-confetti";
+import { HotKeys } from "react-hotkeys";
 
-export default App;
+// internal imports
+import "./App.css";
+
+export function App() {
+    const keyMap = {
+        KEYS_SEQUENCE: "c s s b a t t l e",
+    };
+
+    const handlers = {
+        KEYS_SEQUENCE: () => blastConfetti(),
+    };
+
+    const blastConfetti = () => {
+        confetti();
+    };
+
+    return (
+        <HotKeys keyMap={keyMap} handlers={handlers}>
+            <p className="main-text">Type "cssbattle"</p>
+        </HotKeys>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// external imports
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// internal imports
 import "./index.css";
-import App from "./App";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
