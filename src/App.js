fa10fc72879f87dfbec4b1e494eb1697f83d0a40
mohitdhatrak <msdhatrak25@gmail.com>
2024-04-14 23:08:47 +0530

    feat: added focus on element to detect user key input immediately

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // external imports
 import confetti from "canvas-confetti";
 import { HotKeys } from "react-hotkeys";
+import { useEffect, useRef } from "react";
 
 // internal imports
 import "./App.css";
@@ -16,13 +17,27 @@ export function App() {
     };
 
     const blastConfetti = () => {
+        // get random configuration to blast confetti
         const config = getRandomConfig();
+
         confetti(config);
     };
 
+    const hotkeysRef = useRef(null);
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        // focus only working as expected when it changes from one element to another
+        // that is the reason why here I switch focus from hotkeys element to paragraph element
+        hotkeysRef?.current?.focus();
+        textRef?.current?.focus();
+    }, []);
+
     return (
-        <HotKeys keyMap={keyMap} handlers={handlers}>
-            <p className="main-text">Type "cssbattle"</p>
+        <HotKeys keyMap={keyMap} handlers={handlers} innerRef={hotkeysRef}>
+            <p className="main-text" tabIndex={-1} ref={textRef}>
+                Type "cssbattle"
+            </p>
         </HotKeys>
     );
 }
